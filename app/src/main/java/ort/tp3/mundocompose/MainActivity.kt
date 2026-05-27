241f5c1928e61064d1eb2d76091739e3d95d9808
Agus Salatino <salatino.agus@gmail.com>
2026-05-27 14:42:13 -0300

    feat: initial setup with welcome and login screens

---
+++ app/src/main/java/ort/tp3/mundocompose/MainActivity.kt
@@ -0,0 +1,43 @@
+package ort.tp3.mundocompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.Preview
+import ort.tp3.mundocompose.ui.theme.HolaMundoComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HolaMundoComposeTheme {
+                MainApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainApp() {
+    var currentScreen by remember { mutableStateOf("welcome") }
+
+    when (currentScreen) {
+        "welcome" -> WelcomeScreen(
+            onLoginClick = { currentScreen = "login" }
+        )
+        "login" -> LoginScreen(
+            onBackClick = { currentScreen = "welcome" }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WelcomeScreenPreviewMain() {
+    HolaMundoComposeTheme {
+        MainApp()
+    }
+}
